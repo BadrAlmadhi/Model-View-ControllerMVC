49370e75394e56dd8ed483dd97a8f9a5180d3a8f
Badr Almadi <badr.almadhi95@gmail.com>
2023-07-11 22:22:48 -0600

    added seeding but having a problem with that need to ask a TA

---
+++ seeds/seed.js
@@ -0,0 +1,30 @@
+const sequelize = require('../config/connection');
+const { User, Comment, Blog } = require('../models');
+
+
+const userData = require('./user.json');
+const commentData = require('./comments.json');
+const blogData = require('./blog.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for (const blog of blogData) {
+        await Blog.create({
+            ...blog,
+            user_id: users[Math.floor(Math.random() * users.length)].isSoftDeleted,
+        });
+    }
+
+    const comment = await Comment.bulkCreate(commentData, {
+        returning: true,
+    })
+    process.exit(0);
+}
+
+seedDatabase();
