41e79935af98bc24b7673828a21096ee38b90b65
Badr Almadi <badr.almadhi95@gmail.com>
2023-07-16 17:07:52 -0600

    push for checkin

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening (https //localhost3001)'))
+    app.listen(port, () => console.log('Now listening (https //localhost3001)'))
 });
 
 
